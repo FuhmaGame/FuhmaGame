67ebf3816fb94839780265581036d7ba78dbc712
unknown <FuhmaGame@github.com>
2014-08-29 17:15:09 -0400

    Color cycle now speed based. Some shader junk, and an additive grid  layer.

--- Assets/Scripts/CycleFogColor.cs
+++ Assets/Scripts/CycleFogColor.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class CycleFogColor : MonoBehaviour
 {
-	public float cycleTime = 30.0f;
+	public float speed = 0.1f;
+	public float hue = 0.0f;
 	public float saturation = 1.0f;
 	public float brightness = 1.0f;
 
@@ -12,22 +13,21 @@ public class CycleFogColor : MonoBehaviour
 	{
 		StartCoroutine ("CycleColors");
 	}
-	
-	IEnumerator CycleColors() 
-	{
-		float t = 0.0f;
-		float hue = 0.0f;
 
-		while(t <= cycleTime)
-		{			
-			hue = Mathf.Lerp(0.0f,1.0f,t/cycleTime);
-			RenderSettings.fogColor = new HSBColor(hue,saturation,brightness).ToColor();
-			t += Time.deltaTime;
-			yield return null;
+	void Update()
+	{
+		hue += speed*Time.deltaTime;
+
+		while(hue > 1.0f)
+		{
+			hue -= 1.0f;
 		}
 
-		hue = 1.0f;
+		while(hue <0.0f)
+		{
+			hue += 1.0f;
+		}
 
-		StartCoroutine("CycleC0lors");
+		RenderSettings.fogColor = new HSBColor (hue, saturation, brightness).ToColor ();
 	}
 }

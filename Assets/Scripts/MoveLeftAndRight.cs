e3a9af447ee8b698e5e776df58fb83025942eb73
unknown <FuhmaGame@github.com>
2014-08-30 12:22:03 -0400

    Touch input for main gameplay implemented

--- Assets/Scripts/MoveLeftAndRight.cs
+++ Assets/Scripts/MoveLeftAndRight.cs
@@ -14,7 +14,17 @@ public class MoveLeftAndRight : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		//This moves our object left or right based on input
+		//This moves our object left or right based on keyboard input
 		transform.position += Vector3.right*Input.GetAxis ("Horizontal")*speed*Time.deltaTime;
+
+		if(PseudoInput.Instance.leftPressed)
+		{
+			transform.position += Vector3.left*speed*Time.deltaTime;
+		}
+
+		if(PseudoInput.Instance.rightPressed)
+		{
+			transform.position += Vector3.right*speed*Time.deltaTime;
+		}
 	}
 }

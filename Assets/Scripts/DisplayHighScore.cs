674824750987eac0515e6278f9c81b1833e0ea1f
unknown <FuhmaGame@github.com>
2014-09-03 00:17:39 -0400

    Broken stupid bubble sort wannabe high score nonsense bs

--- Assets/Scripts/DisplayHighScore.cs
+++ Assets/Scripts/DisplayHighScore.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class DisplayHighScore : MonoBehaviour 
 {
+	public int scoreSlot = 0;
 	private string label;
 	// Use this for initialization
 	void Start ()
 	{
 		TextMesh textMesh = GetComponent<TextMesh> ();
 		label = textMesh.text;
-		textMesh.text = label + PlayerPrefs.GetInt ("Highscore");
+		textMesh.text = label + PlayerPrefs.GetInt ("HighScore" + scoreSlot.ToString());
 	}
 }

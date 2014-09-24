1a901bafabcf0580c22cb2ccede4864bca3605ff
unknown <FuhmaGame@github.com>
2014-09-24 15:54:47 -0400

    Collectable stars! Woo! (they still need their own particle effect)

--- Assets/Scripts/GibOnTrigger.cs
+++ Assets/Scripts/GibOnTrigger.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class GibOnTrigger : MonoBehaviour 
 {
 	public GameObject gib;
+	public float scoreValue = 0;
 
 	void OnTriggerEnter()
 	{
 		Instantiate(gib, transform.position, Quaternion.identity);
+		GameManager.Instance.score += scoreValue;
 		Destroy (gameObject);
 	}
 }
